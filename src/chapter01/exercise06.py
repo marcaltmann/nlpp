4dc6a947420aac8962bf0a95fa1ec8702a1d66ae
Marc Altmann <marcaltmann@posteo.de>
2025-10-05 14:54:42 +0200

    Add chart examples

---
+++ src/chapter01/exercise06.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+from nltk.book import *
+
+
+text2.dispersion_plot(["Elinor", "Marianne", "Edward", "Willoughby"])
+plt.show()
--- src/chapter01/exercise22.py
+++ src/chapter01/exercise22.py
@@ -1,5 +1,5 @@
+import matplotlib.pyplot as plt
 from nltk.book import *
-from nltk import FreqDist
 
 samples = [w for w in text5 if len(w) == 4]
 f = FreqDist(samples)
@@ -9,3 +9,6 @@ print(f)
 top50 = f.most_common()[:50]
 
 print(top50)
+
+f.plot(50, cumulative=True)
+plt.show()
